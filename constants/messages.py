a699893024c52b28c74d20431dd727aa3db1937e
Barış <alibrs15@hotmail.com>
2022-01-19 22:13:14 +0300

    custom messages created

--- constants/messages.py
+++ constants/messages.py
@@ -1,2 +1,11 @@
 class CustomMessages:
-    pass
+
+    # Error messages
+    CONNECTION_ERROR = "Connection can not created!"
+    CREATE_TABLE_ERROR = "Table can not created!"
+    DATA_INSERT_ERROR = "Data cannot inserted!"
+    PARAMETER_ERROR = "You dont specify any parameter like File Name and Database Name"
+    JSON_FILE_ERROR = "Json file can not opened"
+
+    #Success messages
+    DATA_INSERT_SUCCES = "Data inserted successfully!"
